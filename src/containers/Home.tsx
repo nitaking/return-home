4bfda8d7e02cf9a81bb907a9ef56b6743da587fb
nitaking <10850034+nitaking@users.noreply.github.com>
2019-01-20 17:25:33 +0900

    :heavy_plus_sign: Add antd-mobile(UI Library)

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,5 +1,12 @@
+/* tslint:disable: no-submodule-imports  */
 import * as React from 'react';
 import Helmet from 'react-helmet';
+import Button from 'antd-mobile/lib/button';
+import 'antd-mobile/lib/button/style/css';
+import WhiteSpace from 'antd-mobile/lib/white-space';
+import 'antd-mobile/lib/white-space/style/css';
+import WingBlank from 'antd-mobile/lib/wing-blank';
+import 'antd-mobile/lib/wing-blank/style/css';
 
 const Home = () => (
   <div>
@@ -18,6 +25,66 @@ const Home = () => (
       ]}
     />
     <h2>Home</h2>
+    <WingBlank>
+      <Button>default</Button>
+      <WhiteSpace />
+      <Button disabled={true}>default disabled</Button>
+      <WhiteSpace />
+
+      <Button type="primary">primary</Button>
+      <WhiteSpace />
+      <Button type="primary" disabled={true}>
+        primary disabled
+      </Button>
+      <WhiteSpace />
+
+      <Button type="warning">warning</Button>
+      <WhiteSpace />
+      <Button type="warning" disabled={true}>
+        warning disabled
+      </Button>
+      <WhiteSpace />
+
+      <Button loading={true}>loading button</Button>
+      <WhiteSpace />
+      <Button icon="check-circle-o">with icon</Button>
+      <WhiteSpace />
+      <Button icon="check-circle-o" inline={true} size="small" style={{ marginRight: '4px' }}>
+        with icon and inline
+      </Button>
+      <Button icon="check-circle-o" inline={true} size="small">
+        with icon and inline
+      </Button>
+      <WhiteSpace />
+
+      {/* <Button activeStyle={false}>无点击反馈</Button><WhiteSpace /> */}
+      {/* <Button activeStyle={{ backgroundColor: 'red' }}>custom feedback style</Button><WhiteSpace /> */}
+
+      <WhiteSpace />
+      <Button type="primary" inline={true} style={{ marginRight: '4px' }}>
+        inline primary
+      </Button>
+      {/* use `am-button-borderfix`. because Multiple buttons inline arranged, the last one border-right may not display */}
+      <Button type="ghost" inline={true} style={{ marginRight: '4px' }} className="am-button-borderfix">
+        inline ghost
+      </Button>
+
+      <WhiteSpace />
+      <Button type="primary" inline={true} size="small" style={{ marginRight: '4px' }}>
+        primary
+      </Button>
+      <Button type="primary" inline={true} size="small" disabled={true}>
+        primary disabled
+      </Button>
+      <WhiteSpace />
+      <Button type="ghost" inline={true} size="small" style={{ marginRight: '4px' }}>
+        ghost
+      </Button>
+      {/* use `am-button-borderfix`. because Multiple buttons inline arranged, the last one border-right may not display */}
+      <Button type="ghost" inline={true} size="small" className="am-button-borderfix" disabled={true}>
+        ghost disabled
+      </Button>
+    </WingBlank>
   </div>
 );
 
